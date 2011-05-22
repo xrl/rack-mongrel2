ba338fd7d2e093a0b21f6c0bb27aaf67e74c2403
Nathan Duran <skg5bknyc8@snkmail.com>
2011-05-21 17:21:31 -0700

    Handle SIGTERM gracefully and (hopefully) correctly.

--- lib/mongrel2/connection.rb
+++ lib/mongrel2/connection.rb
@@ -3,6 +3,9 @@ require 'mongrel2/request'
 require 'mongrel2/response'
 
 module Mongrel2
+  
+  class ConnectionDiedError < StandardError; end
+  
   class Connection
     CTX = ZMQ::Context.new(1)
 
@@ -23,12 +26,16 @@ module Mongrel2
 
     def recv
       msg = nil
-      ready_sockets = ZMQ.select([@reqs], nil, nil, 30)
-      if !ready_sockets.nil?
-        ready_sockets[0].each do | socket |
-          msg = socket.recv(ZMQ::NOBLOCK)
-          msg = Request.parse(msg) unless msg.nil?
+      begin
+        ready_sockets = ZMQ.select([@reqs], nil, nil, 30)
+        if !ready_sockets.nil?
+          ready_sockets[0].each do | socket |
+            msg = socket.recv(ZMQ::NOBLOCK)
+            msg = Request.parse(msg) unless msg.nil?
+          end
         end
+      rescue RuntimeError => e
+        raise ConnectionDiedError
       end
       msg
     end
--- lib/rack/handler/mongrel2.rb
+++ lib/rack/handler/mongrel2.rb
@@ -20,44 +20,45 @@ module Rack
             running = false
           end
         end
-
-        while running do
-          req = conn.recv
-          next if req.nil? || req.disconnect?
-          break if !running
-
-          script_name = ENV['RACK_RELATIVE_URL_ROOT'] || req.headers['PATTERN'].split('(', 2).first.gsub(/\/$/, '')
-          env = {
-            'rack.version' => Rack::VERSION,
-            'rack.url_scheme' => 'http', # Only HTTP for now
-            'rack.input' => StringIO.new(req.body),
-            'rack.errors' => $stderr,
-            'rack.multithread' => true,
-            'rack.multiprocess' => true,
-            'rack.run_once' => false,
-            'mongrel2.pattern' => req.headers['PATTERN'],
-            'REQUEST_METHOD' => req.headers['METHOD'],
-            'SCRIPT_NAME' => script_name,
-            'PATH_INFO' => req.headers['PATH'].gsub(script_name, ''),
-            'QUERY_STRING' => req.headers['QUERY'] || ''
-          }
-
-          env['SERVER_NAME'], env['SERVER_PORT'] = req.headers['host'].split(':', 2)
-          req.headers.each do |key, val|
-            unless key =~ /content_(type|length)/i
-              key = "HTTP_#{key.upcase.gsub('-', '_')}"
-            end
-            env[key] = val
-          end
-
-          status, headers, rack_response = app.call(env)
-          body = ''
-          rack_response.each { |b| body << b }
-          conn.reply(req, body, status, headers)
-        end
         
-        conn.close
-        Process.exit!
+        begin 
+          while running do
+            req = conn.recv
+            next if req.nil? || req.disconnect?
+            break if !running
+
+            script_name = ENV['RACK_RELATIVE_URL_ROOT'] || req.headers['PATTERN'].split('(', 2).first.gsub(/\/$/, '')
+            env = { 'rack.version'        => Rack::VERSION,
+                    'rack.url_scheme'     => 'http', # Only HTTP for now
+                    'rack.input'          => StringIO.new(req.body),
+                    'rack.errors'         => $stderr,
+                    'rack.multithread'    => true,
+                    'rack.multiprocess'   => true,
+                    'rack.run_once'       => false,
+                    'mongrel2.pattern'    => req.headers['PATTERN'],
+                    'REQUEST_METHOD'      => req.headers['METHOD'],
+                    'SCRIPT_NAME'         => script_name,
+                    'PATH_INFO'           => req.headers['PATH'].gsub(script_name, ''),
+                    'QUERY_STRING'        => req.headers['QUERY'] || '' }
+
+            env['SERVER_NAME'], env['SERVER_PORT'] = req.headers['host'].split(':', 2)
+            req.headers.each do |key, val|
+              unless key =~ /content_(type|length)/i
+                key = "HTTP_#{key.upcase.gsub('-', '_')}"
+              end
+              env[key] = val
+            end
+
+            status, headers, rack_response = app.call(env)
+            body = ''
+            rack_response.each { |b| body << b }
+            conn.reply(req, body, status, headers)
+          end
+        rescue ::Mongrel2::ConnectionDiedError => e
+          conn.close
+          exit
+          return
+        end
       end #def self.run
       
     end #class Mongrel2
