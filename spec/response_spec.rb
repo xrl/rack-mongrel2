be805f845e50792a3846b225550fa5f5e4b8307e
Xavier Lange <xrlange@gmail.com>
2013-04-24 23:23:33 -0700

    They expect send

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -9,11 +9,15 @@ describe Mongrel2::Response do
   end
 
   it 'should build the HTTP request format' do
-    @req.should_receive(:uuid) { 'UUID' }
+    @req.should_receive(:uuid)    { 'UUID' }
     @req.should_receive(:conn_id) { 'CONN_ID' }
 
+    thing = double()
+    thing.should_receive(:hi)
+    thing.hi
+
     httpreq = "UUID 7:CONN_ID, HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\nBoo!"
-    @resp.should_receive(:send_string).with(httpreq)
+    @resp.should_receive(:send).with(httpreq)
 
     @response.send_http(@req, 'Boo!', 200, {})
   end
@@ -23,7 +27,7 @@ describe Mongrel2::Response do
     @req.should_receive(:conn_id) { 'CONN_ID' }
 
     httpreq = 'UUID 7:CONN_ID, '
-    @resp.should_receive(:send_string).with(httpreq)
+    @resp.should_receive(:send).with(httpreq)
 
     @response.close(@req)
   end
